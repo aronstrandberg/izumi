0827f9222ac74ee2e3b19316bcf1581610d17b85
Aron Strandberg <strandberg.aron@gmail.com>
2018-01-13 19:27:25 +0100

    Remove Countdown

--- src/components/Timer/index.js
+++ src/components/Timer/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import Countdown from 'react-countdown-now'
 import { startTimer } from 'timer/actions'
 
 class Timer extends Component {
