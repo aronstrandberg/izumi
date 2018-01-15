af50cb57981d5c0c7e6cb6c44a1a8d04e5114577
Aron Strandberg <strandberg.aron@gmail.com>
2018-01-15 10:51:46 +0100

    Add links to header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import css from './styles.css'
 
 function Header() {
   return (
     <header className={css.header}>
-      izumi
+      <Logo />
+      <Navigation />
     </header>
   )
 }
+
+const Navigation = () => (
+  <nav className={css.navigation}>
+    <Link to="/about">About</Link>
+  </nav>
+)
+
+const Logo = () => (
+  <div className={css.logo}>
+    <Link to="/">
+      izumi
+    </Link>
+  </div>
+)
+
 
 export default Header
